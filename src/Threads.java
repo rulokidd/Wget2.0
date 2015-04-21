fe8036558bc63e19ebf0b6c6994816cd2e6acda6
rulokidd <raul.rpb@hotmail.com>
2015-04-21 10:38:25 +0200

    nom = possarNom()
    nom = possarNom()

--- src/Threads.java
+++ src/Threads.java
@@ -113,7 +113,7 @@ public class Threads extends Thread {
 		}
 		
 		if (existeix) {
-			nom.possarNom(url, filtres);//esta mal, nom no tiene ningun metodo que se llame possarNom()
+			nom = possarNom(url, filtres);
 			downloadURL(is, fos, filtres);
 		}
 		
